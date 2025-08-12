320eb50cac168aee718432937e3cce0ab6cef7a4
Dmitriy Ivkov <divkov57@rosetta13.csail.mit.edu>
2025-08-12 13:40:34 -0400

    working on vcf->maf

---
+++ utilities.py
@@ -0,0 +1,10 @@
+
+def assert
+
+def create_bgzip(path):
+
+
+
+if __name__ == "__main__":
+   path1 = "/data/rbg/shared/datasets/TCGA/Genomic/downloads/49fe1ce1-3dc8-4130-a29c-3f3ca82bfe61/c30ef13b-f23a-46c2-9187-f64269318358.wxs.pindel.raw_somatic_mutation.vcf.gz" 
+   path2 = "/data/rbg/shared/datasets/TCGA/Genomic/downloads/49fe1ce1-3dc8-4130-a29c-3f3ca82bfe61/"
